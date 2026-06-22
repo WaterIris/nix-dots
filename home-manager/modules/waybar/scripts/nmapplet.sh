33312bf8e89ad6346174064b511e4b75c9259b57
WaterIris <179727984+WaterIris@users.noreply.github.com>
2026-06-22 20:21:15 +0200

    (update) renamed directory

--- home-manager/modules/waybar/scripts/nmapplet.sh
+++
@@ -1,9 +0,0 @@
-# Check if network manager applet is running
-if pgrep ".nm-connection" > /dev/null
-then
-    # If running, kill it
-    pkill ".nm-connection"
-else
-    # If not running, start it
-    nm-connection-editor &
-fi
